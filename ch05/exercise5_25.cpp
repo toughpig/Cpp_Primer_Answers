9962b3d4b15cd084adcb47590552e8a5d2de7717
toughpig <378981240@qq.com>
2016-09-24 12:11:28 +0800

    连续计算，两次循环

--- ch05/exercise5_25.cpp
+++ ch05/exercise5_25.cpp
@@ -5,25 +5,28 @@ using std::cin; using std::cout; using std::endl; using std::runtime_error;
 int main(void)
 {
 	int i, j;
-	cout << "please input tow numbers: " << endl;
-	while (cin >> i >> j)
+	bool cond=true;
+	while (cond)
 	{
-		try
-		{
-			if (j == 0)
-				throw runtime_error("divisor is 0");
-			cout << i / j << endl;
-		}
-		catch (runtime_error err)
-		{
-			cout << err.what() << "\nTry Again? Enter y or n" << endl;
-			char c;
-			cin >> c;
-			if (c != 'y')
-				break;
-		}
 		cout << "please input tow numbers: " << endl;
+		while (cin >> i >> j)
+		{
+			try
+			{
+				if (j == 0)
+					throw runtime_error("divisor is 0");
+				cout << i / j << endl;
+			}
+			catch (runtime_error err)
+			{
+				cout << err.what() << "\nTry Again? Enter y or n" << endl;
+				char c;
+				cin >> c;
+				if (c != 'y')
+					cond = false;
+					break;
+			}
+		}
 	}
-
 	return 0;
 }
